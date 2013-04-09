700cabff4a32db6f237c8902e9094a5fe3d3fa60
FliX <FliX@95e293ee-8810-49cf-90b1-b57357737a15>
2013-04-09 15:12:49 +0000

    Added kamertje to the project, also added random color generation for teh cubes.
    git-svn-id: https://subversion.assembla.com/svn/vokey/branches/RoomAddBranch@15 95e293ee-8810-49cf-90b1-b57357737a15

--- Client/Assets/Scripts/SpawnCubes.cs
+++ Client/Assets/Scripts/SpawnCubes.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class SpawnCubes : MonoBehaviour
 {
-	int zoffset = 0;
+	public int zoffset = 0;
 	int rows = 0;
 
+    Color[] colors;
+    System.Random r;
 	// Use this for initialization
 	void Start ()
 	{
-
+        colors = new Color[8];
+        colors[0] = Color.blue;
+        colors[1] = Color.cyan;
+        colors[2] = Color.green;
+        colors[3] = Color.magenta;
+        colors[4] = Color.red;
+        colors[5] = Color.yellow;
+        colors[6] = Color.white;
+        colors[7] = Color.black;
+        r = new System.Random();
 	}
 
 	// Update is called once per frame
@@ -35,9 +46,9 @@ public class SpawnCubes : MonoBehaviour
 				// Spawn a cube
 				GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
 				cube.AddComponent<Rigidbody> ();
-				cube.renderer.material.color = Color.blue;
+                cube.renderer.material.color = getRandomColor();
 				cube.transform.localScale = new Vector3 (1, 1, 1);
-				cube.transform.position = new Vector3 (offset, 1, zoffset);
+				cube.transform.position = new Vector3 (offset, 2, zoffset);
 
 				// Add the itembehaviour script
 				cube.AddComponent<ItemSelect> ();
@@ -65,4 +76,9 @@ public class SpawnCubes : MonoBehaviour
 			zoffset = 0;
 		}
 	}
+
+    public Color getRandomColor()
+    {
+        return colors[r.Next(0, colors.GetLength(0))];
+    }
 }
