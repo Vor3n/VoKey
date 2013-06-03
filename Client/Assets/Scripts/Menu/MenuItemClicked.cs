2afefb63f73433c6ffc24f4808fc115efb6adc2a
uhupascal <pascalschotman@hotmail.com>
2013-06-03 12:21:08 +0200

    extended menu and made it do things, also added pause menu

--- Client/Assets/Scripts/Menu/MenuItemClicked.cs
+++ Client/Assets/Scripts/Menu/MenuItemClicked.cs
@@ -12,9 +12,12 @@ public class MenuItemClicked : MonoBehaviour {
 		ShowRooms,
 		ShowStudentResults,
 		ShowStudents,
+		Login,
 		Logout,
 		Quit,
 		CHANGEME,
+		LogoutYes,
+		LogoutNo,
 	}
 	
 	public Item item = Item.CHANGEME;
@@ -32,19 +35,45 @@ public class MenuItemClicked : MonoBehaviour {
 			
 		}
 		
+		if (item == Item.Login)
+		{
+			GameObject usernamelabel = GameObject.Find("UsernameLabel");
+			UILabel UIusernamelabel = (UILabel)usernamelabel.GetComponent("UILabel");
+			string username = UIusernamelabel.text;
+			GameObject passwordlabel = GameObject.Find("PasswordLabel");
+			UILabel UIpasswordlabel = (UILabel)passwordlabel.GetComponent("UILabel");
+			string password = UIpasswordlabel.text;
+			//do real check here
+			if (password == "student" && username == "student")
+			{
+				Application.LoadLevel("RoomTest");
+			}
+			else if (password == "teach" && username == "teach")
+			{
+				Application.LoadLevel("TeacherMenu");
+			}
+			else
+			{
+				Debug.LogError("ELSE EMPTY");
+			}
+		}
+		
 		if (item == Item.Logout)
 		{
-			
+			GameObject surepanel = GameObject.Find("LogoutSurePanel");
+			surepanel.GetComponent<UIPanel>().alpha = 1f;
+			GameObject teachmenu = GameObject.Find("TeacherMainMenu");
+			teachmenu.GetComponent<UIPanel>().alpha = 0f;
 		}
 		
 		if (item == Item.Quit)
 		{
-			
+			Application.Quit();
 		}
 		
 		if (item == Item.ShowOtherRooms)
 		{
-			
+
 		}
 		
 		if (item == Item.ShowClasses)
@@ -70,6 +99,30 @@ public class MenuItemClicked : MonoBehaviour {
 		if (item == Item.ShowStudents)
 		{
 			
+		}	
+		
+		if (item == Item.LogoutNo)
+		{
+			GameObject surepanel = GameObject.Find("LogoutSurePanel");
+			surepanel.GetComponent<UIPanel>().alpha = 0f;
+			try
+			{
+				GameObject teachmenu = GameObject.Find("TeacherMainMenu");
+				teachmenu.GetComponent<UIPanel>().alpha = 1f;
+			}
+			catch{}
+			try
+			{
+				GameObject teachmenu = GameObject.Find("StudentMainMenu");
+				teachmenu.GetComponent<UIPanel>().alpha = 1f;
+			}
+			catch{}
+			//surepanel.GetComponent<UIPanel>().enabled = false;
+		}	
+		
+		if (item == Item.LogoutYes)
+		{
+			Application.LoadLevel("MainMenu");
 		}
 	}
 }
