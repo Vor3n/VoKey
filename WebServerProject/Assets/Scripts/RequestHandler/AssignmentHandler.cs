80eca4a79a8bd4a26295d6f3dc1ab5fc2445d999
Felix Mann <djflix@gmail.com>
2013-06-26 19:11:15 +0200

    Added demo assignments. Guess the song ;). Also probably reformatted StreetHandler.cs. Sorry 'bout that.

--- WebServerProject/Assets/Scripts/RequestHandler/AssignmentHandler.cs
+++ WebServerProject/Assets/Scripts/RequestHandler/AssignmentHandler.cs
@@ -1,12 +1,33 @@
-using System;
+using AssemblyCSharp;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using VokeySharedEntities;
+using Vokey;
+using GuiTest;
 
-namespace AssemblyCSharp
-{
-		public class AssignmentHandler
-		{
-				public AssignmentHandler ()
-				{
-				}
-		}
+public class AssignmentHandler : RequestHandler {
+
+    private static string[] acceptableCommands = { "assignment" };
+
+    public AssignmentHandler(HttpListenerContext hlc)
+        : base(hlc, acceptableCommands)
+    {
+        
+    }
+
+    public override void handleSimpleRequest(string command)
+    {
+	    User studentUser = new User("student", "student", User.UserType.Student);
+	    studentUser.addAssignment(new Assignment("Talk to to corporate", "LIKE A BOSS", System.Guid.NewGuid(), new List<System.Guid>()));
+	    studentUser.addAssignment(new Assignment("Approve Memos", "LIKE A BOSS", System.Guid.NewGuid(), new List<System.Guid>()));
+	    studentUser.addAssignment(new Assignment("Lead a workshop", "LIKE A BOSS", System.Guid.NewGuid(), new List<System.Guid>()));
+	    studentUser.addAssignment(new Assignment("Remember birthdays", "LIKE A BOSS", System.Guid.NewGuid(), new List<System.Guid>()));
+	    studentUser.addAssignment(new Assignment("Direct Workflow", "LIKE A BOSS", System.Guid.NewGuid(), new List<System.Guid>()));
+	    studentUser.addAssignment(new Assignment("My own bathroom", "LIKE A BOSS", System.Guid.NewGuid(), new List<System.Guid>()));
+	    studentUser.addAssignment(new Assignment("Micromanage", "LIKE A BOSS", System.Guid.NewGuid(), new List<System.Guid>()));
+	    studentUser.addAssignment(new Assignment("Promote synergy", "Bake ALL THE CAKE. NOW.", System.Guid.NewGuid(), new List<System.Guid>()));
+	    
+        HttpFunctions.returnXmlStringToHttpClient(context, studentUser.assignments.ToXml());
+    }
 }
-
